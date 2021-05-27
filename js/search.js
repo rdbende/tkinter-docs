12463ea49450142c983d51df2afd6496e50566b0
rdbende <rdbende@gmail.com>
2021-05-27 22:03:28 +0200

    Update

--- js/search.js
+++ js/search.js
@@ -4,7 +4,6 @@ function searchDocsInNavbar() {
     var search = input.value.toUpperCase();
     var ul = document.getElementById("searchUl");
     var li = ul.getElementsByTagName("li");
-    var i;
 
     for (i = 0; i < li.length; i++) {
         var a = li[i].getElementsByTagName("a")[0];
@@ -22,21 +21,54 @@ function searchDocsOnPage() {
     // This function searches only in the title of the cards,
     // also only in the part after the module name
     var input = document.getElementById("searchBar");
-    var search = input.value.toUpperCase();
+    var search = input.value.toLowerCase();
     var ul = document.getElementById("searchUl");
     var li = ul.getElementsByTagName("li");
-    var i;
+    var resultLabel = document.getElementById("resultNumber");
+    var visibles = 0;
 
     for (i = 0; i < li.length; i++) {
         var a = li[i].getElementsByTagName("a")[0];
         var div = a.getElementsByTagName("div")[0];
         var title = div.getElementsByClassName("link__title")[0];
         var txtValue = title.innerHTML.split(".")[1] || title.innerHTML;
+        
 
-        if (txtValue.toUpperCase().indexOf(search) > -1) {
-            li[i].style.display = "";
+        if (txtValue.toLowerCase().indexOf("no result!") > -1) {
+            li[i].remove()
+        } else if (txtValue.toLowerCase().indexOf(search) > -1) {
+            li[i].style.display = "initial";
+            visibles++;
         } else {
             li[i].style.display = "none";
         }
     }
+
+    if (visibles < 1) {
+        var noResult = document.createElement("li");
+        noResult.innerHTML = `<a style="cursor: default;" id="noResult"><div class="card link__card">
+                                    <p class="link__title">No result!</p>
+                                    <p class="link__desc">There seems to be no match for "`
+                                    + input.value
+                                    + `"</p></div></a>`;
+        ul.appendChild(noResult);
+    }
+
+    if (li.length != visibles & visibles != 0) {
+        if (visibles > 1) {
+            resultLabel.innerHTML = visibles + " results";
+        } else {
+            resultLabel.innerHTML = visibles + " result";
+        }
+    } else {
+        resultLabel.innerHTML = ""
+    }
+}
+
+function searchDocsOnLoad() {
+    var input = document.getElementById("searchBar");
+    const searchString = window.location.search;
+    const parameters = new URLSearchParams(searchString);
+    input.value = parameters.get('q').toLowerCase();
+    searchDocsOnPage();
 }
